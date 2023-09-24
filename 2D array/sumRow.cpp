39583e1bcbbbbbde86f22a1645eb43ce76a5a81f
Aman <amanprajapati7266@gmail.com>
2023-09-24 13:04:05 +0530

    add

---
+++ 2D array/sumRow.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int arr[3][3]={{1,2,3} , {6,7,8},{4,8,5}};
+    for(int i=0; i<3; i++){
+        int sum=0;
+        for(int j=0; j<3; j++){
+           sum += arr[i][j]; 
+
+        }
+       cout << sum << " ";
+    }
+}
---
+++ 2D array/tempCodeRunnerFile.cpp
@@ -0,0 +1,3 @@
+
+        for(int j=0; j<3; j++){
+            cout << arr[i][j]
