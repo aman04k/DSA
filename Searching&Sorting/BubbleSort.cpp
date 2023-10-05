88f645c409dbbeeddcb4268c0f2f4eeb76a797b9
Aman <amanprajapati7266@gmail.com>
2023-10-05 21:17:06 +0530

    add

--- Searching&Sorting/BubbleSort.cpp
+++ Searching&Sorting/BubbleSort.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main(){
     int arr[]={10,1,7,6,14};
     int n=sizeof(arr) / sizeof(arr[0]);
-     for(int i=1; i<n; i++){
+     for(int i=1 ; i<n; i++){
         for( int j=0; j<n-i; j++){
             if(arr[j] > arr[j+1]){
                 swap(arr[j], arr[j+1]);
---
+++ Searching&Sorting/InsertionSort.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main(){
+     int arr[]= {10,1,7,4,8,2,11};
+     int n=sizeof(arr) / sizeof(arr[0]);
+      
+    return 0;
+}
