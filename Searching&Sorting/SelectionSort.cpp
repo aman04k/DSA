9c65ece18813f7fa5c9823af83c27d636eb408da
Aman <amanprajapati7266@gmail.com>
2023-10-05 13:15:22 +0530

    add

--- Searching&Sorting/SelectionSort.cpp
+++ Searching&Sorting/SelectionSort.cpp
@@ -5,7 +5,7 @@ int main(){
     int n=sizeof(arr) / sizeof(arr[0]);
     for(int i=0; i<n-1; i++){
         int minIndex=i;
-        for(int j=i+1; j<n; j++){
+        for(int j=i +1; j<n; j++){
             if(arr[j]<arr[minIndex])
             minIndex=j;
         }
