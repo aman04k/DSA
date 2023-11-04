6647a59993a2ad18d6bf2405fd384e17c43d50fe
Aman <amanprajapati7266@gmail.com>
2023-11-04 22:28:06 +0530

    add

--- Pointer/charArray.cpp
+++ Pointer/charArray.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 using namespace std;
-int main(){
+int main(){ 
     char ch[10]="aman";
     char* c=ch;
     cout<<c<<endl;
+    cout << ch << endl;
+    cout << &ch << endl;
+    cout << *c << endl; 
     return 0;
 }
