a5d1346d6cb1d2d84d3ca06cb0f2c548e3b53d34
Aman <amanprajapati7266@gmail.com>
2023-11-04 22:24:07 +0530

    add

---
+++ Pointer/arrayFunction.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int arr[4]={10,20,23,45};
+    cout << arr <<endl;
+    cout << &arr << endl;
+    cout << arr[0] << endl;
+    return 0;
+
+}
---
+++ Pointer/tempCodeRunnerFile.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main(){
+    char ch[10]="aman";
+    char* c=ch;
+    cout<<c<<endl;
+    return 0;
+}
