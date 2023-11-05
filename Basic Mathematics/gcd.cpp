6b8e81a80a8b3f695baa63681db5a0ecc1b6bab0
Aman <amanprajapati7266@gmail.com>
2023-11-05 13:13:40 +0530

    add

---
+++ Basic Mathematics/gcd.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int gcd(int a, int b) {
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
+}
+
+int main() {
+    int num1, num2;
+    
+    cout << "Enter two numbers: ";
+    cin >> num1 >> num2;
+
+    int gcd_result = gcd(num1, num2);
+
+    cout << "GCD of " << num1 << " and " << num2 << " is: " << gcd_result << std::endl;
+
+    return 0;
+}
